e006fb06b32826a3f8d1c7d8e4a5372ef04d0f5c
Francesco <francesco.argentieri89@gmail.com>
2017-01-15 10:09:51 +0100

    Update Automatic.h

--- automatic.h
+++ automatic.h
@@ -8,14 +8,13 @@ public:
 	Automatic();//costruttore
 	//set input 
 	void setInputSensor(float p);
-	void setInputLine(uint8_t pDX,
-						  uint8_t pSX);
+	void setInputLine(uint8_t pDX, uint8_t pSX);
 	//input function
-	void CheckObstacle();										//controlla ostacoli sul percoso
-	void ReadPath();											//restituisce un tipo percoso letto dai linefollower
+	void CheckObstacle();					//controlla ostacoli sul percoso
+	void ReadPath();					//restituisce un tipo percoso letto dai linefollower
 	//exit function
-	void SpeedControl();										//verifica che il valore di al motore non superi 255
-	void PathError ();											//valuta l'errore sul percoso 
+	void SpeedControl();				        //verifica che il valore di al motore non superi 255
+	void PathError ();				        //valuta l'errore sul percoso 
 	//get 
 	int getPathError(); 
 	bool getCheckObstacle();
